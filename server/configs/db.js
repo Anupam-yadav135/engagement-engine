04c1bdb4bf2c6f9d41649833f000ca6bac1b87c4
Anupam <anupamprashantyadav123@gmail.com>
2026-01-11 18:46:00 +0530

    error correction in db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async ()=>{
      try{
         mongoose.connection.on('connected', ()=> console.log("database connected"))
-        await mongoose.connect(`${process.env.MONGODB_URL}/ Linkup`)
+        await mongoose.connect(`${process.env.MONGODB_URL}/Linkup`)
      }catch(error){
         console.log(error.message);
      }
